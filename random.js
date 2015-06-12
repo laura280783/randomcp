4d39f306add9c8935c7e1fa52c4ca5c11cb8b893
laura280783 <islaslga@gmail.com>
2015-06-12 13:13:40 -0500

    random version 1

---
+++ random.js
@@ -0,0 +1,14 @@
+//Math.random() devuelve número aleatorio entre 0 y 1.
+
+var numero = Math.random();
+
+var str = ' MAYOR que 0.5 \n';
+
+if(numero<=0.5)
+{
+  str= ' MENOR que 0.5 \n';
+}
+
+
+console.log('\n' + numero + str + ' n');
+
